491c6d52d1fac848edd2fb66e72871cce0c97286
Àlvar Pérez <alvarperezrelano@gmail.com>
2025-02-21 14:26:34 +0100

    Add catalan language

--- lib/store/language-store.ts
+++ lib/store/language-store.ts
@@ -1,28 +1,30 @@
-import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
-import { en } from '../i18n/locales/en'
-import { es } from '../i18n/locales/es'
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { en } from "../i18n/locales/en";
+import { es } from "../i18n/locales/es";
+import { ca } from "../i18n/locales/ca";
 
-type Language = 'en' | 'es'
+type Language = "en" | "es" | "ca";
 
 type LanguageStore = {
-  language: Language
-  translations: typeof en | typeof es
-  setLanguage: (lang: Language) => void
-}
+  language: Language;
+  translations: typeof en | typeof es | typeof ca;
+  setLanguage: (lang: Language) => void;
+};
 
 export const useLanguageStore = create<LanguageStore>()(
   persist(
     (set) => ({
-      language: 'es',
+      language: "es",
       translations: es,
-      setLanguage: (lang) => set({ 
-        language: lang, 
-        translations: lang === 'en' ? en : es 
-      }),
+      setLanguage: (lang) =>
+        set({
+          language: lang,
+          translations: lang === "en" ? en : lang === "ca" ? ca : es,
+        }),
     }),
     {
-      name: 'language-storage',
+      name: "language-storage",
     }
   )
-) 
+);
